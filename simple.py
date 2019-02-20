4fd8356b2332b62c5b73cf9c37c8dd50ac2701fb
Neeraj Menon <neeraj.menon.m@gmail.com>
2019-02-20 23:35:40 +0000

    Update variable names

--- simple.py
+++ simple.py
@@ -1,16 +1,16 @@
 ﻿import subprocess
 from netmiko import Netmiko
 
-src = input("Enter source ip: ")
-dst = input("Enter dst ip: :")
+src_ip = input("Enter source ip: ")
+dst_ip = input("Enter dst ip: ")
 
-#result = subprocess.call(['traceroute',src]).split('\n')
+#If tracerouting from jumphost to src
+#result = subprocess.call(['traceroute',src_ip]).split('\n')
 
-r2 = Netmiko(ip=src,username="admin",password="noc",secret='noc',device_type="cisco_ios",)
+src = Netmiko(ip=src_ip,username="admin",password="noc",secret='noc',device_type="cisco_ios",)
 
-#r2.enable();
-print(r2.find_prompt())
-trace = r2.send_command('traceroute '+dst)
+print(src.find_prompt())
+trace = src.send_command('traceroute '+dst_ip)
 path = trace.split('\n')[4:]
 path = [i.split()[1] for i in path]
 print("Hops: ",path)
@@ -24,7 +24,7 @@ for device in devices:
     print(device.find_prompt())
     print(device.send_command('show ip int br | include up'))
 
-r2.disconnect()
+src.disconnect()
 for r in devices:
     r.disconnect()
 print("Disconnected from all devices")
