0cf6b1ed8e11e02f21fe48f1ced58c6eb6ce2a29
Neeraj Menon <neeraj.menon.m@gmail.com>
2019-03-20 16:14:51 +0000

    added memory kpi

---
+++ proc_mem.py
@@ -0,0 +1,28 @@
+﻿import subprocess
+from netmiko import Netmiko
+
+src_ip = '10.7.14.7'
+
+src = Netmiko(ip=src_ip,username="rit",password="pan",device_type="cisco_ios",)
+
+print("Connected")
+
+ret = src.send_command("show proc mem | include Pool Total")
+
+def mb(str):
+    return round(int(str)/1024/1024,2)
+
+def percent(a,b):
+    return round((int(a)/int(b)) * 100,2)
+
+memory = dict()
+vals = ret.split(' ')
+memory.update({'processor':{'total':mb(vals[4]),'used':mb(vals[8]),'free':mb(vals[11][0:-2]),'percent':percent(vals[8],vals[4])},'io':{'total':mb(vals[22]),'used':mb(vals[27]),'free':mb(vals[32]),'percent':percent(vals[27],vals[22])}})
+print(memory)
+
+
+
+
+
+
+
