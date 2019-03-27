97f13d56967fb93f7f2c4597e22cda3532adfb03
Neeraj Menon <neeraj.menon.m@gmail.com>
2019-03-27 16:36:57 +0000

    fixed parse eroor

--- proc_mem.py
+++ proc_mem.py
@@ -1,12 +1,12 @@
 from netmiko import Netmiko
 
-src_ip = '10.7.14.7'
+src_ip = '10.1.7.1'
 
 src = Netmiko(ip=src_ip,username="rit",password="pan",device_type="cisco_ios",)
 
 print("Connected")
 
-ret = src.send_command("show proc mem | include Pool Total")
+ret = src.send_command("show proc mem | include Processor Pool | I/O Pool")
 
 def mb(str):
     return round(int(str)/1024/1024,2)
@@ -17,10 +17,11 @@ def percent(a,b):
 memory = dict()
 temp_vals = ret.split(' ')
 vals = []
+
 for string in temp_vals:
     if len(string.strip())>0:
         vals.append(string.strip("\n"))
-        
+print(vals)  
 memory.update({'processor':{'total':mb(vals[3]),'used':mb(vals[5]),'free':mb(vals[7]),'percent':percent(vals[5],vals[3])},'io':{'total':mb(vals[11]),'used':mb(vals[13]),'free':mb(vals[15]),'percent':percent(vals[13],vals[11])}})
 print(memory)
 
