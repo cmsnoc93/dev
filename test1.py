b883cd58131e8fec127f228b30534fbc01f295e2
Neeraj Menon (neemenon) <neemenon@cisco.com>
2019-04-09 09:46:47 +0530

    fixing multithreading

--- test1.py
+++ test1.py
@@ -628,14 +628,13 @@ def index():
 
 ff=0
 def fetchKPI(ssh,nme):
-
-		#show version
-		boo=True
-		global ff
-		while boo:
-			ff=0
-	        try:
-	            ret=ssh.send_command("sh version",use_textfsm=True)
+        #show version
+        boo=True
+        global ff
+        while boo:
+            ff=0
+            try:
+            ret=ssh.send_command("sh version",use_textfsm=True)
 	            print("Sh version Exec")
 	            boo=False
 	        except Exception as e:
